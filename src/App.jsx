c73760a111fa4b7969a2e686ce5b112953338304
andres <andres2345v@gmail.com>
2025-03-25 22:34:54 -0500

    Second commit

--- src/App.jsx
+++ src/App.jsx
@@ -2,18 +2,13 @@ import React, { useState } from 'react'
 import './index.css'
 import { Cronometrer } from './assets/components/Layouts/cronometrer/Cronometrer';
 import { LikeDislike } from './assets/components/Layouts/LikeDislike/LikeDislike';
+import { ColorGenerater } from './assets/components/Layouts/ColorGenerater/ColorGenerater';
 
 export const App = () => {
 // Dark/Light Mode
 const [isDarkMode, setIsDarkMode] = useState(false);
 
-// Color Generator
-const coloresAbsolutos = [
-  "#FF0000", "#00FF00", "#0000FF", "#FFFF00", 
-  "#FF00FF", "#00FFFF", "#000000", "#FFFFFF", 
-  "#808080", "#800000", "#808000", "#800080", 
-  "#008080", "#C0C0C0", "#FFA500", "#A52A2A"
-];
+
 const [selectedColor, setSelectedColor] = useState(null);
 
 // Toggle Dark/Light Mode
@@ -21,12 +16,6 @@ const toggleDarkMode = () => {
   setIsDarkMode(!isDarkMode);
 };
 
-
-// Color Generator Function
-const generateRandomColor = () => {
-  const randomColor = coloresAbsolutos[Math.floor(Math.random() * coloresAbsolutos.length)];
-  setSelectedColor(randomColor);
-};
 
 
 return (
@@ -44,24 +33,9 @@ return (
 
       {/* Like/Dislike Buttons */}
       <LikeDislike />
+
       {/* Color Generator */}
-      <div >
-        <h2 className="text-xl font-bold mb-4">Color Generator</h2>
-        <button 
-          onClick={generateRandomColor} 
-          className="w-full py-2 bg-orange-500 text-white rounded-md hover:bg-orange-600"
-        >
-          Generate Color
-        </button>
-        {selectedColor && (
-          <div 
-            className="mt-4 p-4 text-center rounded-md"
-            style={{ backgroundColor: selectedColor }}
-          >
-            Selected Color: {selectedColor}
-          </div>
-        )}
-      </div>
+      <ColorGenerater />
 
       {/* Timer */}
       <Cronometrer />
